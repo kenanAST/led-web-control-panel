79af4ab40feafbdf74b1d9a2b9e1c13ee19bfec0
kenannn <kenanjoshua3454@gmail.com>
2022-02-26 04:29:34 +0800

    frontend: layout and logic

--- front-end/src/components/ControlPanel.vue
+++ front-end/src/components/ControlPanel.vue
@@ -1,9 +1,9 @@
 <template>
 	<div class="outerBound">
 		<v-card elevation="24" color="#11131e" shaped tile>
-			<div v-for="led in leds" :key="led.id">
+			<div class="grid-container" v-for="led in leds" :key="led.id">
 				<v-btn
-					class="buttons"
+					class="buttons grid-item"
 					v-if="led.enabled"
 					v-bind:color="enabledColor"
 					@click="toggleButton(led.id)"
@@ -12,6 +12,14 @@
 				</v-btn>
 				<v-btn
 					class="buttons"
+					v-else-if="led.id == 0 || led.id == 7"
+					color="#11131e"
+					elevation="0"
+					@click="toggleButton(led.id)"
+				>
+				</v-btn>
+				<v-btn
+					class="buttons grid-item"
 					v-else
 					v-bind:color="disabledColor"
 					@click="toggleButton(led.id)"
@@ -50,30 +58,36 @@
 </template>
 
 <script>
+import io from 'socket.io-client';
+
 export default {
 	data: () => ({
 		testdata: 'JOSHUA KENAN CINCHES',
 		enabledColor: '#1e8d74',
 		disabledColor: '#e23058',
 		enabledToggle: 'Enabled',
-		leds: [
-			{ id: 0, enabled: true },
-			{ id: 1, enabled: false },
-			{ id: 2, enabled: true },
-			{ id: 3, enabled: true },
-			{ id: 4, enabled: true },
-			{ id: 5, enabled: true },
-			{ id: 6, enabled: true },
-			{ id: 7, enabled: true },
-			{ id: 8, enabled: true },
-		],
+		leds: [],
 	}),
 
 	methods: {
 		toggleButton(id) {
+			this.leds.forEach((led) => {
+				led.enabled = false;
+			});
 			const identifiedLED = this.leds.find((led) => led.id === id);
 			identifiedLED.enabled = !identifiedLED.enabled;
+			this.socket.emit('toggleLED', identifiedLED.id + 1, this.leds);
 		},
+	},
+
+	created() {
+		this.socket = io('http://192.168.1.8:8000/');
+	},
+
+	mounted() {
+		this.socket.on('led', (data) => {
+			this.leds = data;
+		});
 	},
 };
 </script>
@@ -81,14 +95,20 @@ export default {
 <style scoped>
 .outerBound {
 	margin: auto;
-	max-width: 60%;
-	padding: 5%;
+	max-width: 100%;
+	padding-top: 25%;
+}
+
+.grid-container {
+	display: inline-grid;
+	grid-template-columns: auto auto auto;
 }
 
 .buttons {
-	margin: 20px;
-	padding: 20px;
-	max-width: 10%;
+	margin: 15px;
+	padding: 40px;
+	width: 40px;
+	max-width: 40px;
 	color: white;
 }
 </style>

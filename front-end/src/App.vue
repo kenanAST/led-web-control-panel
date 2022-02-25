55e7024f262d1dbab2e793dff48249f3853fc1d5
kenannn <kenanjoshua3454@gmail.com>
2022-02-25 17:47:23 +0800

    frontend: Color Pallete

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,26 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<control-panel />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import ControlPanel from './components/ControlPanel.vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	name: 'App',
+
+	components: {
+		HelloWorld,
+		ControlPanel,
+	},
+
+	data: () => ({
+		again: 'TOPPPP HELLO WORLD',
+	}),
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+	background-color: #0f1219;
 }
 </style>
---
+++ front-end/src/components/ControlPanel.vue
@@ -0,0 +1,38 @@
+<template>
+	<div>
+		<v-card elevation="24" color="#11131e" shaped tile>
+			<v-btn class="buttons" color="#1e8d74"> {{ enabledToggle }} </v-btn>
+			<v-btn class="buttons" color="#00897B"> {{ enabledToggle }} </v-btn>
+			<v-btn class="buttons" color="#e23058"> {{ enabledToggle }} </v-btn>
+			<v-btn class="buttons" color="#e23058"> {{ enabledToggle }} </v-btn>
+			<v-btn class="buttons" color="#e23058"> {{ enabledToggle }} </v-btn>
+			<v-btn class="buttons" color="#e23058"> {{ enabledToggle }} </v-btn>
+			<v-btn class="buttons" color="#e23058"> {{ enabledToggle }} </v-btn>
+			<v-btn class="buttons" color="#e23058"> {{ enabledToggle }} </v-btn>
+			<v-btn class="buttons" color="#e23058"> {{ enabledToggle }} </v-btn>
+		</v-card>
+	</div>
+</template>
+
+<script>
+export default {
+	data: () => ({
+		testdata: 'JOSHUA KENAN CINCHES',
+		enabledColor: '#e23058',
+		enabledToggle: 'Enabled',
+	}),
+};
+</script>
+
+<style scoped>
+div {
+	margin: auto;
+	max-width: 60%;
+	padding: 5%;
+}
+
+.buttons {
+	margin: 20px;
+	color: white;
+}
+</style>
